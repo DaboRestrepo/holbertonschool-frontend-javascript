806058ee2732e10d338f32f803c46ae759313cf4
DaboRestrepo <daborestrepo@gmail.com>
2022-08-01 10:29:10 -0500

    Use async, await and try

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,30 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const person = {
+    status: 'pending',
+    value: '',
+  };
+  const picture = {
+    status: 'pending',
+    value: '',
+  };
+  try {
+    const getIn = await signUpUser(firstName, lastName);
+    person.status = 'fulfilled';
+    person.value = getIn;
+  } catch (err) {
+    person.status = 'rejected';
+    person.value = err.toString();
+  }
+  try {
+    const pictureUp = await uploadPhoto(fileName);
+    picture.status = 'fulfilled';
+    picture.value = pictureUp;
+  } catch (err) {
+    picture.status = 'rejected';
+    picture.value = err.toString();
+  }
+  return [person, picture];
+}
