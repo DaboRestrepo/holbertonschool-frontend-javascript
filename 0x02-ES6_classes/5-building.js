7d1d637cd9a752aa7f855227781fbd4f7947ee6e
DaboRestrepo <daborestrepo@gmail.com>
2022-08-01 12:35:48 -0500

    fix file 5 and override the evacuationWarningMessage method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building
-      && this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+      && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
